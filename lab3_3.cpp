e6855a62b94b5e7ddefbe50a1f7f475ca0fed0ae
lab303-7 <lab303-7@labeng.local>
2019-01-11 17:14:56 +0700

    first commit

---
+++ lab3_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x=2;
+    double y=69;
+    double ans = 0;
+
+    while(1/x>=1/y){
+        ans=ans+(1/x);
+        x=x+1;
+           
+    }
+    cout<<ans;
+    return 0;
+}
